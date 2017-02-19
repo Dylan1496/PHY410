5f28cb121b744ce49a29624b76c6bf3da7a82000
Dylan Delgado <ddelgado@live.com>
2017-02-19 16:22:46 -0500

    Problem 2 done

---
+++ Homework2/arithmetic.cpp
@@ -0,0 +1,58 @@
+//Problem 2 of HW 2:
+
+#include <iostream>
+int main() {
+  //declarations as given in the problem:
+  int i1 = 2, i2 = 5, i3 = -3;
+  double d1 = 2.0, d2 = 5.0, d3 = -0.5;
+
+  //part a:
+  std::cout << i1 + (i2 * i3) << std::endl;
+
+  //part b:
+  std::cout << i1 * (i2 + i3) << std::endl;
+
+  //part c:
+  std::cout << i1 / (i2 + i3) << std::endl;
+
+  //part d:
+  std::cout << i1 / i2 + i3 << std::endl;
+
+  //part e:
+  std::cout << 3 + 4 + 5 / 3 << std::endl;
+
+  //part f:
+  std::cout << (3 + 4 + 5) / 3 << std::endl;
+
+  //part g:
+  std::cout << d1 + (d2 * d3) << std::endl;
+
+  //part h:
+  std::cout << d1 + d2 * d3 << std::endl;
+
+  //part i:
+  std::cout << d1 / d2 - d3 << std::endl;
+
+  //part j:
+  std::cout << d1 / (d2 - d3) << std::endl;
+
+  //part k:
+  std::cout << d1 + d2 + d3 / 3 << std::endl;
+
+  //part l:
+  std::cout << (d1 + d2 + d3) / 3 << std::endl;
+
+  //part m:
+  std::cout << d1 + d2 + (d3 / 3) << std::endl;
+
+  //part n:
+  std::cout << 3 * (d1 + d2) * (d1 - d3) << std::endl;
+
+  //end of program
+
+  return 0;
+}
+
+  
+
+
